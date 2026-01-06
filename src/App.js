b80e11a342eebdf9aaf9d5f0aba5c1c3e97e9c1e
idomaori98 <idoma@mta.ac.il>
2026-01-06 18:27:46 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+    return (
+        <div className="App">
+            <Counter />
+        </div>
+    );
+}
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    const date = new Date();
+    date.setDate(date.getDate() + count);
+    const [step, setStep] = useState(1);
+
+    return (
+        <>
+            <div>
+                <button onClick={() => setStep((s) => s - 1)}>-</button>
+                <span>Step : {step}</span>
+                <button onClick={() => setStep((s) => s + 1)}>+</button>
+            </div>
+            <div>
+                <button onClick={() => setCount((c) => c - step)}>-</button>
+                <span>Count : {count}</span>
+                <button onClick={() => setCount((c) => c + step)}>+</button>
+            </div>
+            <p>
+                <span>
+                    {count === 0
+                        ? "Today is"
+                        : count > 0
+                        ? `${count} days from today is `
+                        : `${Math.abs(count)} days ago was `}
+                </span>
+                <span>{date.toDateString()}</span>
+            </p>
+        </>
+    );
+}
