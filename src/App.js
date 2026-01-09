20c97fe59abac65f2cf62a394fe6d05b0f49a4c4
idomaori98 <idoma@mta.ac.il>
2026-01-09 21:18:16 +0200

    adding step bar and reset btn

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import e from "cors";
 import "./styles.css";
 import { useState } from "react";
 
@@ -15,28 +16,49 @@ function Counter() {
     date.setDate(date.getDate() + count);
     const [step, setStep] = useState(1);
 
+    function handleReset() {
+        setCount(0);
+        setStep(1);
+    }
+
     return (
-        <>
+        <div>
             <div>
-                <button onClick={() => setStep((s) => s - 1)}>-</button>
+                <input
+                    type="range"
+                    min="0"
+                    max="10"
+                    value={step}
+                    onChange={(e) => setStep(Number(e.target.value))}
+                />
                 <span>Step : {step}</span>
-                <button onClick={() => setStep((s) => s + 1)}>+</button>
             </div>
             <div>
                 <button onClick={() => setCount((c) => c - step)}>-</button>
-                <span>Count : {count}</span>
+                <input
+                    type="text"
+                    value={count}
+                    onChange={(e) => setCount(Number(e.target.value))}
+                />
                 <button onClick={() => setCount((c) => c + step)}>+</button>
             </div>
             <p>
                 <span>
                     {count === 0
-                        ? "Today is"
+                        ? "Today is "
                         : count > 0
                         ? `${count} days from today is `
                         : `${Math.abs(count)} days ago was `}
                 </span>
                 <span>{date.toDateString()}</span>
             </p>
-        </>
+            {count !== 0 || step !== 1 ? (
+                <div>
+                    <button onClick={handleReset}>Reset</button>
+                </div>
+            ) : (
+                ""
+            )}
+        </div>
     );
 }
